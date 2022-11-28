9ea953849b29209041e74ff70d6607a4aea53d84
Giulliano Siviero <gsilvazanottisiviero@scu.edu>
2022-11-27 22:18:35 -0800

    Update IP

--- System.ts
+++ System.ts
@@ -1,5 +1,5 @@
 import { Arguments } from "./Models/Arguments";
 import { SelfSite } from "./Models/SelfSite";
-
+var ip = require("ip");
 const port = new Arguments("-p",8080,process.argv);
-const site = new SelfSite("localhost",port.value);
+const site = new SelfSite(ip.address(),port.value);
