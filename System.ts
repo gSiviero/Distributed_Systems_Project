ba066a6e121344da3d19bce9a5ac73ddffa249b6
Giulliano Siviero <gsilvazanottisiviero@scu.edu>
2022-11-28 17:02:39 -0800

    Adjust Default port to 2000

--- System.ts
+++ System.ts
@@ -1,5 +1,7 @@
 import { Arguments } from "./Models/Arguments";
 import { SelfSite } from "./Models/SelfSite";
+import * as config from "./systemConfig.json";
+
 var ip = require("ip");
-const port = new Arguments("-p",8080,process.argv);
+const port = new Arguments("-p",config.port,process.argv);
 const site = new SelfSite(ip.address(),port.value);
